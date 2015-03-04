d03474b4ff73153edd6df986ad49c1645876d42a
A.J. Fraser <ajamiesonfraser@gmail.com>
2015-03-04 13:38:51 -0400

    added additions to twitter and goole+ strategies

--- config/passport.js
+++ config/passport.js
@@ -104,43 +104,61 @@ module.exports = function(passport) {
 
             consumerKey     : configAuth.twitterAuth.consumerKey,
             consumerSecret  : configAuth.twitterAuth.consumerSecret,
-            callbackURL     : configAuth.twitterAuth.callbackURL
-
+            callbackURL     : configAuth.twitterAuth.callbackURL,
+            passReqToCallback : true
         },
-        function(token, tokenSecret, profile, done) {
+        function(req, token, tokenSecret, profile, done) {
 
             // make the code asynchronous
             // User.findOne won't fire until we have all our data back from Twitter
             process.nextTick(function() {
 
-                User.findOne({ 'twitter.id' : profile.id }, function(err, user) {
+                if (!req.user) {
+                    User.findOne({'twitter.id': profile.id}, function (err, user) {
 
-                    // if there is an error, stop everything and return that
-                    // ie an error connecting to the database
-                    if (err)
-                        return done(err);
+                        // if there is an error, stop everything and return that
+                        // ie an error connecting to the database
+                        if (err)
+                            return done(err);
 
-                    // if the user is found then log them in
-                    if (user) {
-                        return done(null, user); // user found, return that user
-                    } else {
-                        // if there is no user, create them
-                        var newUser                 = new User();
+                        // if the user is found then log them in
+                        if (user) {
+                            return done(null, user); // user found, return that user
+                        } else {
+                            // if there is no user, create them
+                            var newUser = new User();
 
-                        // set all of the user data that we need
-                        newUser.twitter.id          = profile.id;
-                        newUser.twitter.token       = token;
-                        newUser.twitter.username    = profile.username;
-                        newUser.twitter.displayName = profile.displayName;
+                            // set all of the user data that we need
+                            newUser.twitter.id = profile.id;
+                            newUser.twitter.token = token;
+                            newUser.twitter.username = profile.username;
+                            newUser.twitter.displayName = profile.displayName;
 
-                        // save our user into the database
-                        newUser.save(function(err) {
-                            if (err)
-                                throw err;
-                            return done(null, newUser);
-                        });
-                    }
-                });
+                            // save our user into the database
+                            newUser.save(function (err) {
+                                if (err)
+                                    throw err;
+                                return done(null, newUser);
+                            });
+                        }
+                    });
+                }else {
+                    // user already exists and is logged in, we have to link accounts
+                    var user            = req.user; // pull the user out of the session
+
+                    // update the current users twitter credentials
+                    user.twitter.id = profile.id;
+                    user.twitter.token = token;
+                    user.twitter.username = profile.username;
+                    user.twitter.displayName = profile.displayName;
+
+                    // save the user
+                    user.save(function(err) {
+                        if (err)
+                            throw err;
+                        return done(null, user);
+                    });
+                }
 
             });
 
@@ -150,41 +168,60 @@ module.exports = function(passport) {
             clientID        : configAuth.googleAuth.clientID,
             clientSecret    : configAuth.googleAuth.clientSecret,
             callbackURL     : configAuth.googleAuth.callbackURL,
+            passReqToCallback : true
 
         },
-        function(token, refreshToken, profile, done) {
+        function(req, token, refreshToken, profile, done) {
 
             // make the code asynchronous
             // User.findOne won't fire until we have all our data back from Google
             process.nextTick(function() {
 
-                // try to find the user based on their google id
-                User.findOne({ 'google.id' : profile.id }, function(err, user) {
-                    if (err)
-                        return done(err);
+                if(!req.user) {
+                    // try to find the user based on their google id
+                    User.findOne({'google.id': profile.id}, function (err, user) {
+                        if (err)
+                            return done(err);
 
-                    if (user) {
+                        if (user) {
 
-                        // if a user is found, log them in
+                            // if a user is found, log them in
+                            return done(null, user);
+                        } else {
+                            // if the user isnt in our database, create a new user
+                            var newUser = new User();
+
+                            // set all of the relevant information
+                            newUser.google.id = profile.id;
+                            newUser.google.token = token;
+                            newUser.google.name = profile.displayName;
+                            newUser.google.email = profile.emails[0].value; // pull the first email
+
+                            // save the user
+                            newUser.save(function (err) {
+                                if (err)
+                                    throw err;
+                                return done(null, newUser);
+                            });
+                        }
+                    });
+                }else {
+                    // user already exists and is logged in, we have to link accounts
+                    var user            = req.user; // pull the user out of the session
+
+                    // update the current users google credentials
+                    user.google.id = profile.id;
+                    user.google.token = token;
+                    user.google.name = profile.displayName;
+                    user.google.email = profile.emails[0].value; // pull the first email
+
+                    // save the user
+                    user.save(function(err) {
+                        if (err)
+                            throw err;
                         return done(null, user);
-                    } else {
-                        // if the user isnt in our database, create a new user
-                        var newUser          = new User();
-
-                        // set all of the relevant information
-                        newUser.google.id    = profile.id;
-                        newUser.google.token = token;
-                        newUser.google.name  = profile.displayName;
-                        newUser.google.email = profile.emails[0].value; // pull the first email
-
-                        // save the user
-                        newUser.save(function(err) {
-                            if (err)
-                                throw err;
-                            return done(null, newUser);
-                        });
-                    }
-                });
+                    });
+                }
             });
 
         }));
