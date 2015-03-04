8f52a3a9dddcdafa65775ef21351e1eb2fbecaea
A.J. Fraser <ajamiesonfraser@gmail.com>
2015-03-04 13:23:24 -0400

    added gitignore and changed auth

--- config/database.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by ajfraser on 15-03-03.
- */
-// config/database.js
-module.exports = {
-
-    'url': 'mongodb://ajfraser:jamieson@proximus.modulusmongo.net:27017/wajo3siH' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-};
